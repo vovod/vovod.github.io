fd6dd3f23de1d119b59d458b2386e832554b9d07
vovod <nguyenhaid0212@gmail.com>
2023-10-06 15:37:59 +0700

    js-for-background-changing

--- script.js
+++ script.js
@@ -15,14 +15,28 @@ function hideMenu() {
 
 // change color on click
 var background_index = 0;
+if (sessionStorage.getItem("background_index") == null) {
+    sessionStorage.setItem("background_index", background_index);
+} else {
+    background_index = sessionStorage.getItem("background_index");
+}
+
+const dir = './images/background/';
+var files = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 function changeBg() {
-    const dir = './images/background/';
+
     var img = document.getElementById("header");
-    var files = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+
     if (background_index < files.length - 1) {
         background_index++;
     } else {
         background_index = 0;
     }
+    sessionStorage.setItem("background_index", background_index);
+    img.style.backgroundImage = "linear-Gradient(rgba(206, 214, 247, 0.7), rgba(51, 57, 81, 0.7)), url(" + dir + files[background_index] + ")";
+}
+function getBg() {
+    var img = document.getElementById("header");
+    var files = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
     img.style.backgroundImage = "linear-Gradient(rgba(206, 214, 247, 0.7), rgba(51, 57, 81, 0.7)), url(" + dir + files[background_index] + ")";
 }
